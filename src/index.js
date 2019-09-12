046d8ef84b5f03b03acbdb98a35ed6380ff01f8a
Olga <o.bondartseva@cdek.ru>
2019-09-12 17:18:10 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,204 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactRouterDOM from 'react-router-dom';
 import './index.css';
+
+
+const Router = ReactRouterDOM.BrowserRouter;
+const Route = ReactRouterDOM.Route;
+const Switch = ReactRouterDOM.Switch;
+const Link = ReactRouterDOM.Link;
+const NavLink = ReactRouterDOM.NavLink;
+
+class Main extends React.Component{
+    render(){
+        return <h2>Главная</h2>;
+    }
+}
+
+class About extends React.Component{
+    render(){
+        const id= this.props.match.params.id;
+        const name = this.props.match.params.name;
+
+        return <h2>Id: {id}, Name: {name}</h2>;
+    }
+}
+class NotFound extends React.Component{
+    render(){
+        return <h2>Ресурс не найден</h2>;
+    }
+}
+
+
+const propsValues = {
+    title: "List of phones",
+    items: [
+        "Iphone 10",
+        "HTC U",
+        "Samsung"
+    ]
+}
+
+function Item(props) {
+    return <li>{props.name}</li>
+}
+
+class SearchPlugin extends React.Component {
+    constructor(props) {
+        super(props);
+        this.onTextChanged = this.onTextChanged.bind(this)
+    }
+
+    onTextChanged(e) {
+        let text = e.target.value.trim();
+        this.props.filter(text);
+    }
+
+    render () {
+        return <input type="text" placeholder="Search" onChange={this.onTextChanged}/>
+    }
+}
+
+class ItemList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {items: this.props.data.items};
+
+        this.filterList = this.filterList.bind(this);
+    }
+
+    filterList(text) {
+        let filteredList = this.props.data.items.filter(function(item) {
+            return item.toLowerCase().search(text.toLowerCase()) !== -1;
+        })
+
+        this.setState({items: filteredList});
+    }
+
+    render () {
+        return (
+            <div>
+                <h2>{this.props.data.title}</h2>
+                <SearchPlugin filter={this.filterList}/>
+                <ul>
+                    {
+                        this.state.items.map(function(item) {
+                            return <Item key={item} name={item}/>
+                        })
+                    }
+                </ul>
+            </div>
+        );
+    }
+}
+
+/*ReactDOM.render(
+    <ItemList data={propsValues}/>,
+    document.getElementById("list")
+)*/
+
+const phones =[
+    {id: 1, name: "iPhone 7"},
+    {id: 2, name: "Google Pixel"},
+    {id: 3, name: "HTC U Ultra"}
+];
+
+class Phone extends React.Component {
+    render() {
+        const prodId = this.props.match.params.id;
+        let phone;
+        for (var i=0; i<phones.length; i++) {
+            if (phones[i].id === prodId) {
+                phone = phones[i];
+                break;
+            }
+        }
+
+        if (phone === undefined) {
+            return <p>Товар не найден</p>;
+        } else {
+            return <p>Товар {phone.name}</p>
+        }
+    }
+}
+
+class PhonesList extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Список товаров</h2>
+                <ul>
+                    {
+                        phones.map(function(item) {
+                            return <li key={item.id}>
+                                <NavLink to={`/products/phones/${item.id}`}>{item.name}</NavLink>
+                            </li>
+
+                        })
+                    }
+                </ul>
+            </div>
+
+
+        )
+    }
+}
+
+class Tablet extends React.Component {
+    render() {
+        return <h3>Tablets</h3>
+    }
+}
+
+class Products extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Товары</h2>
+                <nav>
+                    <Link to="/products/phones">PhonesList</Link>
+                    <Link to="/products/tablets">Tablet</Link>
+                </nav>
+                <Switch>
+                    <Route exact path="/products/phones" component={PhonesList}/>
+                    <Route path="/products/tablets" component={Tablet}/>
+                    <Route path="/products/phones/:id" component={Phone}/>
+                </Switch>
+            </div>
+        )
+    }
+}
+
+class Nav extends React.Component {
+    render() {
+        return (
+            <nav>
+                <NavLink exact to="/" activeClassName="active">Главная</NavLink>
+                <NavLink to="/about/1/About" activeClassName="active">О сайте</NavLink>
+                <NavLink to="/itemList" activeClassName="active">Список</NavLink>
+                <NavLink to="/products" activeClassName="active">Товары</NavLink>
+            </nav>
+        )
+    }
+}
+
+ReactDOM.render (
+    <Router>
+        <div>
+            <Nav/>
+            <Switch>
+                <Route exact path="/" component={Main}/>
+                <Route path="/about/:id/:name" component={About}/>
+                <Route path="/itemList" children={()=><ItemList data={propsValues}/>}/>
+                <Route path="/products" component={Products}/>
+                <Route component={NotFound}/>
+            </Switch>
+        </div>
+    </Router>,
+    document.getElementById("menu")
+)
 
 function tick() {
     ReactDOM.render(
@@ -99,73 +296,6 @@ class ClickButton extends React.Component {
 ReactDOM.render(
     <ClickButton/>,
     document.getElementById("button")
-)
-
-const propsValues = {
-    title: "List of smartphones",
-    items: [
-        "Iphone 10",
-        "HTC U",
-        "Samsung"
-    ]
-}
-
-function Item(props) {
-    return <li>{props.name}</li>
-}
-
-class SearchPlugin extends React.Component {
-    constructor(props) {
-        super(props);
-        this.onTextChanged = this.onTextChanged.bind(this)
-    }
-
-    onTextChanged(e) {
-        let text = e.target.value.trim();
-        this.props.filter(text);
-    }
-
-    render () {
-        return <input type="text" placeholder="Search" onChange={this.onTextChanged}/>
-    }
-}
-
-class ItemList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {items: this.props.data.items};
-
-        this.filterList = this.filterList.bind(this);
-    }
-
-    filterList(text) {
-        let filteredList = this.props.data.items.filter(function(item) {
-            return item.toLowerCase().search(text.toLowerCase()) !== -1;
-        })
-
-        this.setState({items: filteredList});
-    }
-
-    render () {
-        return (
-            <div>
-                <h2>{this.props.data.title}</h2>
-                <SearchPlugin filter={this.filterList}/>
-                <ul>
-                    {
-                        this.state.items.map(function(item) {
-                            return <Item key={item} name={item}/>
-                        })
-                    }
-                </ul>
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(
-    <ItemList data={propsValues}/>,
-    document.getElementById("list")
 )
 
 class AgeField extends React.Component {
